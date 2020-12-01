da8119a7d9f33661fbbd90cef599f63deb55e0bc
zhangsan <zhangsan@sohu.com>
2020-12-01 09:29:44 +0800

    comment func3

---
+++ src/views/article/components/comment-item.vue
@@ -0,0 +1,85 @@
+<template>
+  <van-cell
+    class="comment-item"
+  >
+    <van-image
+      slot="icon"
+      round
+      fit="cover"
+      class="avatar"
+      :src="comment.aut_photo"
+    />
+    <div slot="title">
+      <div class="title-wrap">
+        <div class="name">{{ comment.aut_name }}</div>
+        <div class="like-wrap">
+          <van-icon class="like-icon" name="good-job-o" />
+          <span class="like-count">{{ comment.like_count }}</span>
+        </div>
+      </div>
+      <div class="content">{{ comment.content }}</div>
+      <div>
+        <span
+          class="pubdate"
+        >{{ comment.pubdate | datetime('MM:DD HH:mm') }}</span>
+        <van-button
+          class="reply-btn"
+          round
+          size="mini"
+        >{{ comment.reply_count }}回复</van-button>
+      </div>
+    </div>
+  </van-cell>
+</template>
+
+<script>
+export default {
+  name: 'CommentItem',
+  components: {},
+  props: {
+    comment: {
+      type: Object,
+      required: true
+    }
+  },
+  data () {
+    return {}
+  },
+  computed: {},
+  watch: {},
+  created () {},
+  mounted () {},
+  methods: {}
+}
+</script>
+
+<style scoped lang="less">
+.comment-item {
+  .avatar {
+    width: 36px;
+    height: 36px;
+    margin-right: 13px;
+  }
+  .name {
+    font-size: 14px;
+    color: #406599;
+  }
+  .content {
+    font-size: 16px;
+    color: #222222;
+  }
+  .pubdate {
+    font-size: 10px;
+    margin-right: 10px;
+  }
+  .title-wrap {
+    display: flex;
+    justify-content: space-between;
+  }
+  .like-wrap {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+  }
+}
+</style>
